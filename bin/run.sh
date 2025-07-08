83537dde37b93a4129c998c2e69768c1e10cf7d1
Adarsh Senghani <adarshco077@gmail.com>
2025-07-08 15:27:22 +0530

    fix: correct docker remove command syntax in run script

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,3 @@
 docker build -t paisa-agent .
-docker remove paisa-agent -f
+docker rm -f paisa-agent
 docker run --name paisa-agent --add-host=host.docker.internal:host-gateway --env-file .env -p 8002:8002 -d paisa-agent
